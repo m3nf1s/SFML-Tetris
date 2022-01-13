e288b13e6fae204cd5997474b08dafda9a45132e
m3nf1s <titan94@mail.ru>
2022-01-13 14:26:37 +0300

    Definition and Declaration of Functions

--- SFML-Tetris/GameView.cpp
+++ SFML-Tetris/GameView.cpp
@@ -1,1 +1,24 @@
 #include "GameView.h"
+
+void GameView::Render(sf::RenderWindow& window)
+{
+    window.clear();
+
+    RenderGamefield(window);
+    RenderFigure(window);
+
+    window.display();
+}
+
+const UIPersistenceGameData& GameView::GetUIGameData() const
+{
+    return game_data_;
+}
+
+void GameView::RenderFigure(sf::RenderWindow& window)
+{
+}
+
+void GameView::RenderGamefield(sf::RenderWindow& window)
+{
+}
--- SFML-Tetris/GameView.h
+++ SFML-Tetris/GameView.h
@@ -1,5 +1,20 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include "UIPersistenceGameData.h"
+
 class GameView
 {
+public:
+    void Render(sf::RenderWindow& window);
+    const UIPersistenceGameData& GetUIGameData() const;
+
+private:
+    void RenderFigure(sf::RenderWindow& window);
+    void RenderGamefield(sf::RenderWindow& window);
+
+private:
+    UIPersistenceGameData game_data_;
 };
 
