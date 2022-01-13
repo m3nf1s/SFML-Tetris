70bf5435212f1610d242f7971f7697405dffd055
m3nf1s <titan94@mail.ru>
2022-01-13 21:08:07 +0300

    Refactoring and Implementation
    Added implementation of RenderFigure function
    Duplicated code from RenderFigure and RenderGamefield was moved to GetNewColor function

--- SFML-Tetris/GameView.cpp
+++ SFML-Tetris/GameView.cpp
@@ -9,12 +9,12 @@ GameView::GameView()
             static_cast<float>(game_data_.CELL_SIZE - 1)));
     }
 }
-void GameView::Render(sf::RenderWindow& window, const Gamefield& gamefield)
+void GameView::Render(sf::RenderWindow& window, const Gamefield& gamefield, Figure* figure)
 {
     window.clear();
 
     RenderGamefield(window, gamefield);
-    RenderFigure(window);
+    RenderFigure(window, figure);
 
     window.display();
 }
@@ -24,48 +24,72 @@ const UIPersistenceGameData& GameView::GetUIGameData() const
     return game_data_;
 }
 
-void GameView::RenderFigure(sf::RenderWindow& window)
+void GameView::RenderFigure(sf::RenderWindow& window, Figure* figure)
 {
+
+    uint32_t current_position_row    = figure->GetCurrentPosition().row;
+    uint32_t number_rows             = figure->number_rows;
+    uint32_t current_position_column = figure->GetCurrentPosition().column;
+    uint32_t number_columns          = figure->number_columns;
+
+    for (uint32_t row = current_position_row; row < current_position_row + number_rows; ++row)
+    {
+        for (uint32_t column = current_position_column; column < current_position_column + number_columns; ++column)
+        {
+            cell_.setFillColor(GetNewColor(
+                figure->GetFigure()[row - current_position_row][column - current_position_column]));
+
+            cell_.setPosition(
+                static_cast<float>(game_data_.CELL_SIZE * column),
+                static_cast<float>(game_data_.CELL_SIZE * row));
+
+            window.draw(cell_);
+        }
+    }
 }
 
 void GameView::RenderGamefield(sf::RenderWindow& window, const Gamefield& gamefield)
-{    
-    for (size_t i = 0; i < gamefield.ROWS; ++i)
+{
+    uint32_t number_rows    = gamefield.ROWS;
+    uint32_t number_columns = gamefield.COLUMNS;
+
+    for (uint32_t row = 0; row < number_rows; ++row)
     {
-        for (size_t j = 0; j < gamefield.COLUMNS; ++j)
+        for (uint32_t column = 0; column < number_columns; ++column)
         {
-            switch (gamefield.GetField()[i][j])
-            {
-            case 1:
-                cell_.setFillColor(sf::Color::Cyan);
-                break;
-            case 2:
-                cell_.setFillColor(sf::Color::Green);
-                break;
-            case 3:
-                cell_.setFillColor(sf::Color::Magenta);
-                break;
-            case 4:
-                cell_.setFillColor(sf::Color::Red);
-                break;
-            case 5:
-                cell_.setFillColor(sf::Color::White);
-                break;
-            case 6:
-                cell_.setFillColor(sf::Color::Yellow);
-                break;
-            case 7:
-                cell_.setFillColor(sf::Color::Black);
-                break;
-            default:
-                cell_.setFillColor(sf::Color::Blue);
-                break;
-            }
+            //uint32_t current_value = gamefield.GetField()[row][column];
+            //cell_.setFillColor(GetNewColor(current_value));
+
+            cell_.setFillColor(GetNewColor(gamefield.GetField()[row][column]));
 
             cell_.setPosition(sf::Vector2f(
-                static_cast<float>(game_data_.CELL_SIZE * j),
-                static_cast<float>(game_data_.CELL_SIZE * i)));
+                static_cast<float>(game_data_.CELL_SIZE * column),
+                static_cast<float>(game_data_.CELL_SIZE * row)));
+
             window.draw(cell_);
         }
+    }
+}
+
+sf::Color GameView::GetNewColor(uint32_t value) const
+{
+    switch (value)
+    {
+    case 1:
+        return sf::Color::Cyan;
+    case 2:
+        return sf::Color::Green;
+    case 3:
+        return sf::Color::Magenta;
+    case 4:
+        return sf::Color::Red;
+    case 5:
+        return sf::Color::White;
+    case 6:
+        return sf::Color::Yellow;
+    case 7:
+        return sf::Color::Black;
+    default:
+        return sf::Color::Blue;
     }
 }
--- SFML-Tetris/GameView.h
+++ SFML-Tetris/GameView.h
@@ -4,20 +4,23 @@
 
 #include "UIPersistenceGameData.h"
 #include "Gamefield.h"
+#include "Figure.h"
 
 class GameView
 {
 public:
     GameView();
-    void Render(sf::RenderWindow& window, const Gamefield& gamefield);
+    void Render(sf::RenderWindow& window, const Gamefield& gamefield, Figure* figure);
     const UIPersistenceGameData& GetUIGameData() const;
 
 private:
-    void RenderFigure(sf::RenderWindow& window);
+    void RenderFigure(sf::RenderWindow& window, Figure* figure);
     void RenderGamefield(sf::RenderWindow& window, const Gamefield& gamefield);
 
+    sf::Color GetNewColor(uint32_t value) const;
+
 private:
-    UIPersistenceGameData game_data_;
-    sf::RectangleShape    cell_;
+    UIPersistenceGameData   game_data_;
+    sf::RectangleShape      cell_;
 };
 
