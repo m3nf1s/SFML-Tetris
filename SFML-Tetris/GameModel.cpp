17cc85b86af90f630e99a1595edf672ca21b15da
m3nf1s <titan94@mail.ru>
2022-01-13 21:09:09 +0300

    Implementation of functions and default constructor

--- SFML-Tetris/GameModel.cpp
+++ SFML-Tetris/GameModel.cpp
@@ -1,4 +1,11 @@
+#include <random>
+
 #include "GameModel.h"
+
+GameModel::GameModel()
+{
+    SetFigure();
+}
 
 Gamefield& GameModel::GetGamefield()
 {
@@ -8,4 +15,52 @@ Gamefield& GameModel::GetGamefield()
 const Gamefield& GameModel::GetGamefield() const
 {
     return gamefield_;
+}
+
+Figure* GameModel::GetCurrentFigure() const
+{
+    return current_figure_.get();
+}
+
+Figure GameModel::GetRandomFigure()
+{
+    if (figures_.size() != 0)
+    {
+    	std::random_device random_device;
+    	std::mt19937 generator(random_device());
+    	std::uniform_int_distribution<> distribution(0, figures_.size() - 1);
+
+    	return figures_[distribution(generator)];
+    }
+
+    return Figure();
+
+    //return figures_[0];
+}
+
+void GameModel::SetFigure()
+{
+    if (current_figure_ == nullptr)
+    {
+        current_figure_ = std::make_unique<Figure>(GetRandomFigure());
+    }
+
+    current_figure_->SetNewPosition(0, gamefield_.COLUMNS / 2 - 1);
+
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+    std::uniform_int_distribution<uint32_t> distribution(1, 7);
+
+    uint32_t new_value = distribution(generator);
+
+    for (uint32_t row = 0; row < current_figure_->number_rows; ++row)
+    {
+        for (uint32_t column = 0; column < current_figure_->number_columns; ++column)
+        {
+            if (current_figure_->GetFigure()[row][column] == 1)
+            {
+                current_figure_->GetFigure()[row][column] = new_value;
+            }
+        }
+    }
 }
--- SFML-Tetris/GameModel.h
+++ SFML-Tetris/GameModel.h
@@ -1,15 +1,83 @@
 #pragma once
 
+#include <memory>
+
 #include "Gamefield.h"
+#include "Figure.h"
 
 class GameModel
 {
 public:
+    GameModel();
+
     Gamefield& GetGamefield();
 
     const Gamefield& GetGamefield() const;
 
+    Figure* GetCurrentFigure() const;
+
 private:
-    Gamefield gamefield_;
+    Figure GetRandomFigure();
+    void   SetFigure();
+
+private:
+    Gamefield               gamefield_;
+    std::unique_ptr<Figure> current_figure_;
+
+    const std::vector<Figure> figures_ =
+    {
+        std::vector<std::vector<uint32_t>>(
+            {
+                {1, 1},
+                {1, 1},
+                {0, 0},
+                {0, 0}
+            }),
+
+        std::vector<std::vector<uint32_t>>(
+            {
+                {0, 1},
+                {0, 1},
+                {0, 1},
+                {0, 1}
+            }),
+
+        std::vector<std::vector<uint32_t>>(
+            {
+                {1, 0},
+                {1, 1},
+                {1, 0},
+                {0, 0}
+            }),
+        std::vector<std::vector<uint32_t>>(
+            {
+                {0, 1},
+                {0, 1},
+                {1, 1},
+                {0, 0}
+            }),
+        std::vector<std::vector<uint32_t>>(
+            {
+                {1, 0},
+                {1, 0},
+                {1, 1},
+                {0, 0}
+            }),
+        std::vector<std::vector<uint32_t>>(
+            {
+                {1, 0},
+                {1, 1},
+                {0, 1},
+                {0, 0}
+            }),
+        std::vector<std::vector<uint32_t>>(
+            {
+                {0, 1},
+                {1, 1},
+                {1, 0},
+                {0, 0}
+            })
+    };
+
 };
 
