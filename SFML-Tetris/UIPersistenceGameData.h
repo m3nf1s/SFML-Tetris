0130746e0ef86bff86a7a875c4edac2cbdaa1416
m3nf1s <titan94@mail.ru>
2022-01-13 14:13:16 +0300

    UI Persistence Game Data
    Added Class with Persistence data for UI

---
+++ SFML-Tetris/UIPersistenceGameData.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdint>
+
+class UIPersistenceGameData
+{
+public:
+    const uint32_t WINDOW_WIDTH  = 600;
+    const uint32_t WINDOW_HEIGHT = 800;
+    const uint32_t CELL_SIZE     = 40;
+};
+
