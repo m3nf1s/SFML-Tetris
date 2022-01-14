5d63b499240b5d9990b664ab52acc275701a9b19
m3nf1s <titan94@mail.ru>
2022-01-14 20:36:25 +0300

    Update GameView.cpp
    Update RenderFigure method

--- SFML-Tetris/GameView.cpp
+++ SFML-Tetris/GameView.cpp
@@ -26,24 +26,28 @@ const UIPersistenceGameData& GameView::GetUIGameData() const
 
 void GameView::RenderFigure(sf::RenderWindow& window, Figure* figure)
 {
-
     uint32_t current_position_row    = figure->GetCurrentPosition().row;
-    uint32_t number_rows             = figure->number_rows;
     uint32_t current_position_column = figure->GetCurrentPosition().column;
+
+    uint32_t number_rows             = figure->number_rows;
     uint32_t number_columns          = figure->number_columns;
 
     for (uint32_t row = current_position_row; row < current_position_row + number_rows; ++row)
     {
         for (uint32_t column = current_position_column; column < current_position_column + number_columns; ++column)
         {
-            cell_.setFillColor(GetNewColor(
-                figure->GetFigure()[row - current_position_row][column - current_position_column]));
+            uint32_t current_value = figure->GetFigure()[row - current_position_row][column - current_position_column];
+            
+            if (current_value != 0)
+            {
+                cell_.setFillColor(GetNewColor(current_value));
 
-            cell_.setPosition(
-                static_cast<float>(game_data_.CELL_SIZE * column),
-                static_cast<float>(game_data_.CELL_SIZE * row));
+                cell_.setPosition(
+                    static_cast<float>(game_data_.CELL_SIZE * column),
+                    static_cast<float>(game_data_.CELL_SIZE * row));
 
-            window.draw(cell_);
+                window.draw(cell_);
+            }
         }
     }
 }
@@ -87,8 +91,6 @@ sf::Color GameView::GetNewColor(uint32_t value) const
         return sf::Color::White;
     case 6:
         return sf::Color::Yellow;
-    case 7:
-        return sf::Color::Black;
     default:
         return sf::Color::Blue;
     }
