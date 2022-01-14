c93324160d13596ca09fe6bde4157df9d5145d5f
m3nf1s <titan94@mail.ru>
2022-01-14 20:36:53 +0300

    Move logic
    Added move logic with boundary checking

--- SFML-Tetris/GameController.cpp
+++ SFML-Tetris/GameController.cpp
@@ -1,1 +1,72 @@
 #include "GameController.h"
+#include "GameModel.h"
+#include "GameView.h"
+#include "GameInstance.h"
+
+GameController::GameController(GameModel* new_game_model, GameView* new_game_view)
+    : game_model_(new_game_model)
+    , game_view_(new_game_view)
+{
+}
+
+void GameController::PerformLogic(sf::RenderWindow& window)
+{
+    sf::Event event;
+    if (window.pollEvent(event))
+    {
+        PerformEvent(window, event);
+    }
+    if (timer_.asSeconds() > delay_)
+    {
+        game_model_->GetCurrentFigure()->SetNewPosition(
+            game_model_->GetCurrentFigure()->GetCurrentPosition().row + 1,
+            game_model_->GetCurrentFigure()->GetCurrentPosition().column);
+
+        timer_ = clock_.restart();
+    }
+    else
+    {
+        timer_ += clock_.getElapsedTime();
+    }
+    game_view_->Render(window, game_model_->GetGamefield(), game_model_->GetCurrentFigure());
+}
+
+void GameController::PerformEvent(sf::RenderWindow& window, const sf::Event& event)
+{
+    if (event.type == sf::Event::Closed || event.key.code == sf::Keyboard::Escape)
+    {
+        window.close();
+    }
+
+    if (event.type == sf::Event::KeyPressed)
+    {
+        uint32_t current_row    = game_model_->GetCurrentFigure()->GetCurrentPosition().row;
+        uint32_t current_column = game_model_->GetCurrentFigure()->GetCurrentPosition().column;
+
+        if (event.key.code == sf::Keyboard::Enter)
+        {
+            game_model_->SetNewFigure();
+        }
+        if (event.key.code == sf::Keyboard::Left)
+        {
+            if (current_column > 0)
+            {
+                current_column -= 1;
+                game_model_->GetCurrentFigure()->SetNewPosition(current_row, current_column);
+            }
+        }
+        if (event.key.code == sf::Keyboard::Right)
+        {
+            if (current_column < game_model_->GetGamefield().COLUMNS - game_model_->GetCurrentFigure()->number_columns)
+            {
+                current_column += 1;
+                game_model_->GetCurrentFigure()->SetNewPosition(current_row, current_column);
+            }
+        }        
+        if (event.key.code == sf::Keyboard::Down)
+        {
+            current_row += 1;
+            game_model_->GetCurrentFigure()->SetNewPosition(current_row, current_column);
+        }
+    }    
+}
--- SFML-Tetris/GameController.h
+++ SFML-Tetris/GameController.h
@@ -1,5 +1,18 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
 class GameController
 {
+public:
+    GameController(class GameModel* new_game_model, class GameView* new_game_view);
+    void PerformLogic(sf::RenderWindow& window);
+    void PerformEvent(sf::RenderWindow& window, const sf::Event& event);
+private:
+    class GameModel*  game_model_;
+    class GameView*   game_view_;
+
+    float       delay_ = 5000.0f;
+    sf::Clock   clock_;
+    sf::Time    timer_ = clock_.restart();
 };
 
