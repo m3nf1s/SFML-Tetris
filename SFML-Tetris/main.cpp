373df26f57affc32ef5f13fdda6c30dfcf6b9e65
m3nf1s <titan94@mail.ru>
2022-01-21 22:08:56 +0300

    Update main.cpp
    Refactoring

--- SFML-Tetris/main.cpp
+++ SFML-Tetris/main.cpp
@@ -1,9 +1,14 @@
 #include "GameInstance.h"
+#include <memory.h>
 
 int main()
 {
-    GameInstance* instance = GameInstance::GetGameInstance();
+    //GameInstance* instance = GameInstance::GetGameInstance();
+    //instance->Start();
+    //delete instance;
+
+    std::unique_ptr<GameInstance> instance(GameInstance::GetGameInstance());
     instance->Start();
-    delete instance;
+
     return 0;
 }
